6a94932e6661f4732f7c0bc16065047549edfc9b
Akshay Aurora <akshayaurora@yahoo.com>
2019-06-08 16:50:24 -0700

    fetch and render park markers

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,36 +1,98 @@
 import * as WebBrowser from "expo-web-browser";
-import MapView from "react-native-maps";
+import MapView, { Marker, ProviderPropType } from 'react-native-maps';
 import React from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View, Dimensions } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
+const { width, height } = Dimensions.get('window');
 
-export default function HomeScreen(props) {
-  toSearch = () => {
-    props.navigation.navigate("Search");
+import markerImg from '../assets/images/marker.png';
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const SPACE = 0.01;
+
+export default class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      coordinates: [{
+         latitude: 37.78825,
+         longitude: -122.4324
+      },
+      {
+         latitude: 47.78825,
+         longitude: -122.4324
+      }]
+    };
+  }
+
+  getParks() {
+    return fetch('http://api.nemus.world/parks')
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        this.setState({coordinates: json });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+  
+  componentDidMount() {
+    console.log("componentDidMount");
+    this.getParks();
+  }
+  
+  toSearch() {
+    this.props.navigation.navigate("Search");
   };
-  return (
-    <View style={styles.container}>
-      <MapView
-        // ref={c => (this.mapView = c)}
-        style={{ flex: 1, height: 200 }}
-        moveOnMarkerPress={true}
-        showsUserLocation={true}
-        showsMyLocationButton={true}
-        followsUserLocation={true}
-        showsCompass={true}
-      />
-      <TouchableOpacity style={{ height: 40 }} onPress={toSearch}>
-        <Text style={{ fontSize: 30, color: "blue", textAlign: "center" }}>
-          Not there?
-        </Text>
-      </TouchableOpacity>
-    </View>
-  );
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          // ref={c => (this.mapView = c)}
+          style={{ flex: 1, height: 200 }}
+          moveOnMarkerPress={true}
+          showsUserLocation={true}
+          showsMyLocationButton={true}
+          followsUserLocation={true}
+          showsCompass={true}
+          onCalloutPress={() => {console.log("calloutPress");}}>
+          {this.state.coordinates.map(function(item, i){
+            return <Marker
+              key={i}
+              coordinate={item}
+              centerOffset={{ x: -18, y: -60 }}
+              anchor={{ x: 0.69, y: 1 }}
+              image={markerImg}
+              title={item.name}
+              description={item.name}
+            >
+            <Text style={styles.marker}>X</Text>
+            </Marker>;
+          })}
+          
+            
+          </MapView>
+        <TouchableOpacity style={{ height: 40 }} onPress={this.toSearch}>
+          <Text style={{ fontSize: 30, color: "blue", textAlign: "center" }}>
+            Not there?
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+  
+  navigationOptions = {
+    header: null
+  };
 }
 
-HomeScreen.navigationOptions = {
-  header: null
-};
+
 
 const styles = StyleSheet.create({
   container: {
