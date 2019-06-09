7671a638679aa314482a1f29106e9b51a33c7ab2
Akshay Aurora <akshayaurora@yahoo.com>
2019-06-09 09:42:57 -0700

    update trip screen

--- screens/TripScreen.js
+++ screens/TripScreen.js
@@ -1,24 +1,114 @@
 import React from "react";
-import { View, Text, ScrollView } from "react-native";
-import { MaterialIcons } from "@expo/vector-icons";
+import {
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  Image,
+  View,
+  Dimensions,
+  Button
+} from "react-native";
+// import * as AppAuth from "expo-app-auth";
+const { width, height } = Dimensions.get("window");
+import CurrentFireDanger from "../components/CurrentFireDanger";
+import {
+  Feather,
+  MaterialCommunityIcons,
+  Entypo,
+  Foundation,
+  FontAwesome,
+  Ionicons
+} from "@expo/vector-icons";
 
 export default function TripScreen(props) {
+  let data = {
+        "id": 3,
+        "name": "Yosemite Valley",
+        "latitude": 37.721667,
+        "longitude": -119.646389,
+        "imageTag": "1e757e5e-8cbd-46f0-bbca-ab60f21d2386",
+        "dangerLevel": "LOW",
+        "rating": 5,
+        "address": "9035 Village Dr, Yosemite Valley, CA 95389",
+        "website": "nps.gov",
+        "number": "(209) 372-0200",
+        "description": "Iconic national park offers an array of options such as hiking among redwoods & river rafting."
+    }
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ height: 50, backgroundColor: "grey" }}>
-        <Text style={{ color: "white", fontSize: 30 }}>Search Parks</Text>
-      </View>
-      <ScrollView style={{ flex: 1 }}>
-        <View>
-          <Text>ID: {props.navigation.getParam("tripId")}</Text>
-        </View>
-        {[1, 2, 3, 4, 5].map(v => (
-          <View key={v}>
-            <Text>{v}</Text>
+    <ScrollView style={{ flex: 1 }}>
+      <View style={{ flex: 1, flexDirection: "column" }}>
+        <Image
+          style={{ width: width, height: 160 }}
+          source={{
+            uri:
+              `https://storage.googleapis.com/nemus-parks-images/${data.imageTag}`
+          }}
+        />
+
+        <Text
+          style={{
+            fontSize: 32,
+            padding: 5,
+            fontWeight: "bold",
+            marginBottom: 5
+          }}
+        >
+          {data.name}
+        </Text>
+
+        <View style={{ flex: 1, flexDirection: "column", marginBottom: 5 }}>
+          <View style={{ flex: 1, flexDirection: "row", marginBottom: 5 }}>
+            <FontAwesome
+              name="map-marker"
+              size={24}
+              color={"#0056FF"}
+              style={{ width: 36, textAlign: "center" }}
+            />
+            <Text style={{ fontSize: 16 }}>
+              {data.address}
+            </Text>
           </View>
-        ))}
-      </ScrollView>
-    </View>
+
+          {/*<View style={{ flex: 1, flexDirection: "row", marginBottom: 5 }}>
+            <Entypo
+              name="address"
+              size={24}
+              color={"#0056FF"}
+              style={{ width: 36, textAlign: "center" }}
+            />
+            <Text style={{ fontSize: 16 }}>
+              66VF+48 Loma Vista, Big Sur, CA
+            </Text>
+          </View>*/}
+
+          <View style={{ flex: 1, flexDirection: "row", marginBottom: 5 }}>
+            <MaterialCommunityIcons
+              name="web"
+              size={24}
+              color={"#0056FF"}
+              style={{ width: 36, textAlign: "center" }}
+            />
+            <Text style={{ fontSize: 16 }}>{data.website}</Text>
+          </View>
+
+          <View style={{ flex: 1, flexDirection: "row", marginBottom: 5 }}>
+            <Feather
+              name="phone"
+              size={24}
+              color={"#0056FF"}
+              style={{ width: 36, textAlign: "center" }}
+            />
+            <Text style={{ fontSize: 16 }}>{data.number}</Text>
+          </View>
+
+          <Text style={{ marginBottom: 10, padding: 5, marginTop: 10 }}>
+            {data.description}
+          </Text>
+          <CurrentFireDanger />
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
@@ -26,6 +116,6 @@ TripScreen.navigationOptions = {
   header: null,
   drawerLabel: "Current Trip",
   drawerIcon: ({ tintColor }) => (
-    <MaterialIcons name="local-parking" size={24} color={tintColor} />
+    <Foundation name="foot" size={24} color={tintColor} />
   )
 };
